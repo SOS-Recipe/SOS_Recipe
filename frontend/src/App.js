b06e0b3751de6d0828200ef2163a88dfa9a97e61
sam youngs <youngs3@pdx.edu>
2021-03-01 13:45:29 -0800

    Creating bootstrap navbnar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,19 @@
 import logo from './logo.svg';
 import Landing from './components/landing';
+import Header from './components/header';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      < Landing/>
-    </div>
+  <>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"/>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+      <div className="App">
+        < Header/>
+      </div>
+  </>
   );
 }
 

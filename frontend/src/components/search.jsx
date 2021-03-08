5104db0710825672b463c1dd19bb676345ebcfcc
David Froman <dfroman@pdx.edu>
2021-03-07 16:44:21 -0800

    prettied and added dropdowns to landing

--- frontend/src/components/search.jsx
+++ frontend/src/components/search.jsx
@@ -1,94 +1,148 @@
 import React from "react";
-import '../styles/search.css';
+import "../styles/search.css";
 
 class Search extends React.Component {
-    componentDidMount() {
-        document.title="SOS_Recipe"
-    }
-    render() {
-        return(
-            <main>
-                <div class="flex_container">
-                    <div class="search_results_wrapper">
-                        <form class="search_form" name="searchbar">
-                            <input class="search" type="search" placeholder="Search for a recipe" id="search" autofocus required/>
-                            <button type ="submit" class="go_btn">Go</button>
-                            <select name="catagory" id="catagory_drop" class="catagory_drop">
-                                <option value="">Any Catagory</option>
-                                <option value="Beef">Beef</option>
-                                <option value="Breakfast">Breakfast</option>
-                                <option value="Chicken">Chicken</option>
-                                <option value="Dessert">Dessert</option>
-                                <option value="Goat">Goat</option>
-                                <option value="Lamb">Lamb</option>
-                                <option value="Miscellaneous">Miscellaneous</option>
-                                <option value="Pasta">Pasta</option>
-                                <option value="Pork">Pork</option>
-                                <option value="Seafood">Seafood</option>
-                                <option value="Side">Side</option>
-                                <option value="Starter">Starter</option>
-                                <option value="Vegan">Vegan</option>
-                                <option value="Vegetarian">Vegetarian</option>
-                            </select>
-                            <select name="region" id="region_drop" class="region_drop">
-                                <option value="">Any Area</option>
-                                <option value="American">American</option>
-                                <option value="British">British</option>
-                                <option value="Canadian">Canadian</option>
-                                <option value="Chinese">Chinese</option>
-                                <option value="Dutch">Dutch</option>
-                                <option value="Egyptian">Egyptian</option>
-                                <option value="French">French</option>
-                                <option value="Greek">Greek</option>
-                                <option value="Indian">Indian</option>
-                                <option value="Irish">Irish</option>
-                                <option value="Italian">Italian</option>
-                                <option value="Jamaican">Jamaican</option>
-                                <option value="Japanese">Japanese</option>
-                                <option value="Kenyan">Kenyan</option>
-                                <option value="Malaysian">Malaysian</option>
-                                <option value="Mexican">Mexican</option>
-                                <option value="Moroccan">Moroccan</option>
-                                <option value="Polish">Polish</option>
-                                <option value="Portuguese">Portuguese</option>
-                                <option value="Russian">Russian</option>
-                                <option value="Spanish">Spanish</option>
-                                <option value="Thai">Thai</option>
-                                <option value="Tunisian">Tunisian</option>
-                                <option value="Turkish">Turkish</option>
-                                <option value="Unknown">Unknown</option>
-                                <option value="Vietnamese">Vietnamese</option>                               
-                            </select>
-                        </form>
-                        <div class="results_btn">
-                            <button type="button" class="btn border-primary btn-light btn-lg btn-block">Lorem.</button>
-                            <button type="button" class="btn border-primary btn-light btn-lg btn-block">Lorem.</button>
-                            <button type="button" class="btn border-primary btn-light btn-lg btn-block">Lorem.</button>
-                            <button type="button" class="btn border-primary btn-light btn-lg btn-block">Lorem.</button>
-                            <button type="button" class="btn border-primary btn-light btn-lg btn-block">Lorem.</button>
-                            <button type="button" class="btn border-primary btn-light btn-lg btn-block">Lorem.</button>
-                            <button type="button" class="btn border-primary btn-light btn-lg btn-block">Lorem.</button>
-                            <button type="button" class="btn border-primary btn-light btn-lg btn-block">Lorem.</button>
-                            <button type="button" class="btn border-primary btn-light btn-lg btn-block">Lorem.</button>
-                            <button type="button" class="btn border-primary btn-light btn-lg btn-block">Lorem.</button>
-
-                        </div>   
-                    </div>
-                    <div class="output">
-                        <h1>strMeal</h1>
-                        <div class = "ing_meas_wrap">
-                            <p class="ingredients">
-                                strIngredient
-                            </p>
-                            <p class="measurements">
-                                strMeasure
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </main>
-        )
-    }
+  componentDidMount() {
+    document.title = "SOS_Recipe";
+  }
+  render() {
+    return (
+      <main>
+        <div class="flex_container">
+          <div class="search_results_wrapper">
+            <form class="search_form" name="searchbar">
+              <input
+                class="search"
+                type="search"
+                placeholder="Search for a recipe"
+                id="search"
+                autofocus
+                required
+              />
+              <button type="submit" class="go_btn">
+                Go
+              </button>
+              <select name="catagory" id="catagory_drop" class="catagory_drop">
+                <option value="">Any Catagory</option>
+                <option value="Beef">Beef</option>
+                <option value="Breakfast">Breakfast</option>
+                <option value="Chicken">Chicken</option>
+                <option value="Dessert">Dessert</option>
+                <option value="Goat">Goat</option>
+                <option value="Lamb">Lamb</option>
+                <option value="Miscellaneous">Miscellaneous</option>
+                <option value="Pasta">Pasta</option>
+                <option value="Pork">Pork</option>
+                <option value="Seafood">Seafood</option>
+                <option value="Side">Side</option>
+                <option value="Starter">Starter</option>
+                <option value="Vegan">Vegan</option>
+                <option value="Vegetarian">Vegetarian</option>
+              </select>
+              <select name="region" id="region_drop" class="region_drop">
+                <option value="">Any Area</option>
+                <option value="American">American</option>
+                <option value="British">British</option>
+                <option value="Canadian">Canadian</option>
+                <option value="Chinese">Chinese</option>
+                <option value="Dutch">Dutch</option>
+                <option value="Egyptian">Egyptian</option>
+                <option value="French">French</option>
+                <option value="Greek">Greek</option>
+                <option value="Indian">Indian</option>
+                <option value="Irish">Irish</option>
+                <option value="Italian">Italian</option>
+                <option value="Jamaican">Jamaican</option>
+                <option value="Japanese">Japanese</option>
+                <option value="Kenyan">Kenyan</option>
+                <option value="Malaysian">Malaysian</option>
+                <option value="Mexican">Mexican</option>
+                <option value="Moroccan">Moroccan</option>
+                <option value="Polish">Polish</option>
+                <option value="Portuguese">Portuguese</option>
+                <option value="Russian">Russian</option>
+                <option value="Spanish">Spanish</option>
+                <option value="Thai">Thai</option>
+                <option value="Tunisian">Tunisian</option>
+                <option value="Turkish">Turkish</option>
+                <option value="Unknown">Unknown</option>
+                <option value="Vietnamese">Vietnamese</option>
+              </select>
+            </form>
+            <div class="results_btn">
+              <button
+                type="button"
+                class="btn border-primary btn-light btn-lg btn-block"
+              >
+                Lorem.
+              </button>
+              <button
+                type="button"
+                class="btn border-primary btn-light btn-lg btn-block"
+              >
+                Lorem.
+              </button>
+              <button
+                type="button"
+                class="btn border-primary btn-light btn-lg btn-block"
+              >
+                Lorem.
+              </button>
+              <button
+                type="button"
+                class="btn border-primary btn-light btn-lg btn-block"
+              >
+                Lorem.
+              </button>
+              <button
+                type="button"
+                class="btn border-primary btn-light btn-lg btn-block"
+              >
+                Lorem.
+              </button>
+              <button
+                type="button"
+                class="btn border-primary btn-light btn-lg btn-block"
+              >
+                Lorem.
+              </button>
+              <button
+                type="button"
+                class="btn border-primary btn-light btn-lg btn-block"
+              >
+                Lorem.
+              </button>
+              <button
+                type="button"
+                class="btn border-primary btn-light btn-lg btn-block"
+              >
+                Lorem.
+              </button>
+              <button
+                type="button"
+                class="btn border-primary btn-light btn-lg btn-block"
+              >
+                Lorem.
+              </button>
+              <button
+                type="button"
+                class="btn border-primary btn-light btn-lg btn-block"
+              >
+                Lorem.
+              </button>
+            </div>
+          </div>
+          <div class="output">
+            <h1>strMeal</h1>
+            <div class="ing_meas_wrap">
+              <p class="ingredients">strIngredient</p>
+              <p class="measurements">strMeasure</p>
+            </div>
+          </div>
+        </div>
+      </main>
+    );
+  }
 }
 
 export default Search;

9856e4ba0287973b7f6482943771be3d2ef31e67
David Froman <dfroman@pdx.edu>
2021-03-08 09:49:41 -0800

    fixed node modules and the package-lock

--- frontend/src/components/search.jsx
+++ frontend/src/components/search.jsx
@@ -1,145 +1,149 @@
 import React from "react";
-import '../styles/search.css';
-import {Link} from "react-router-dom";
+import "../styles/search.css";
+import { Link } from "react-router-dom";
 
 class Search extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            head_text: "",
-            queryKeys: [],
-            ingredient_items: [],
-            clicked: false,
-            backgroundImg: `url('../images/landing-background.jpg')`
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      head_text: "",
+      queryKeys: [],
+      ingredient_items: [],
+      clicked: false,
+      backgroundImg: `url('../images/landing-background.jpg')`,
+    };
+  }
 
-    componentDidMount() {
-        document.title="SOS_Recipe"
-        console.log(this.props.location.state.data);
-    }
-    over(strMeal){
-      console.log(strMeal)
-      let i = 0;
-      for (i = 0; i < this.props.location.state.data.length; i++){
-        //console.log(this.props.location.state.data[i].strMeal)
-          if(strMeal === this.props.location.state.data[i].strMeal){
-            console.log(this.props.location.state.data[i].strMealThumb)
-            this.setState({
-              head_text: strMeal, 
-              backgroundImg: this.props.location.state.data[i].strMealThumb,
-            })
-            //document.body.style.background = this.props.location.state.data[i].strMealThumb
-            //document.getElementById('mealName').innerHTML = this.props.location.state.data[i].strMeal
-          }
+  componentDidMount() {
+    document.title = "SOS_Recipe";
+    console.log(this.props.location.state.data);
+  }
+  over(strMeal) {
+    console.log(strMeal);
+    let i = 0;
+    for (i = 0; i < this.props.location.state.data.length; i++) {
+      //console.log(this.props.location.state.data[i].strMeal)
+      if (strMeal === this.props.location.state.data[i].strMeal) {
+        console.log(this.props.location.state.data[i].strMealThumb);
+        this.setState({
+          head_text: strMeal,
+          backgroundImg: this.props.location.state.data[i].strMealThumb,
+        });
+        //document.body.style.background = this.props.location.state.data[i].strMealThumb
+        //document.getElementById('mealName').innerHTML = this.props.location.state.data[i].strMeal
       }
     }
+  }
 
-    render() {
-        return(
-            <main>
-                <div class="flex_container">
-                    <div class="search_results_wrapper">
-                        <form class="search_form" name="searchbar">
-                            <input class="search" type="search" placeholder="Search for a recipe" id="search" autofocus required/>
-                            <button type ="submit" class="go_btn">Go</button>
-                            <select name="catagory" id="catagory_drop" class="catagory_drop">
-                                <option value="">Any Catagory</option>
-                                <option value="Beef">Beef</option>
-                                <option value="Breakfast">Breakfast</option>
-                                <option value="Chicken">Chicken</option>
-                                <option value="Dessert">Dessert</option>
-                                <option value="Goat">Goat</option>
-                                <option value="Lamb">Lamb</option>
-                                <option value="Miscellaneous">Miscellaneous</option>
-                                <option value="Pasta">Pasta</option>
-                                <option value="Pork">Pork</option>
-                                <option value="Seafood">Seafood</option>
-                                <option value="Side">Side</option>
-                                <option value="Starter">Starter</option>
-                                <option value="Vegan">Vegan</option>
-                                <option value="Vegetarian">Vegetarian</option>
-                            </select>
-                            <select name="region" id="region_drop" class="region_drop">
-                                <option value="">Any Area</option>
-                                <option value="American">American</option>
-                                <option value="British">British</option>
-                                <option value="Canadian">Canadian</option>
-                                <option value="Chinese">Chinese</option>
-                                <option value="Dutch">Dutch</option>
-                                <option value="Egyptian">Egyptian</option>
-                                <option value="French">French</option>
-                                <option value="Greek">Greek</option>
-                                <option value="Indian">Indian</option>
-                                <option value="Irish">Irish</option>
-                                <option value="Italian">Italian</option>
-                                <option value="Jamaican">Jamaican</option>
-                                <option value="Japanese">Japanese</option>
-                                <option value="Kenyan">Kenyan</option>
-                                <option value="Malaysian">Malaysian</option>
-                                <option value="Mexican">Mexican</option>
-                                <option value="Moroccan">Moroccan</option>
-                                <option value="Polish">Polish</option>
-                                <option value="Portuguese">Portuguese</option>
-                                <option value="Russian">Russian</option>
-                                <option value="Spanish">Spanish</option>
-                                <option value="Thai">Thai</option>
-                                <option value="Tunisian">Tunisian</option>
-                                <option value="Turkish">Turkish</option>
-                                <option value="Unknown">Unknown</option>
-                                <option value="Vietnamese">Vietnamese</option>                               
-                            </select>
-                        </form>
-                        <div class="results_btn">
-                                {this.props.location.state.data.map(recipe => 
-                                <button 
-                                  id={`${recipe.strMeal}`} 
-                                  type="submit" 
-                                  class="btn border-primary btn-light btn-lg btn-block" 
-                                  onMouseOver={()=> this.over(`${recipe.strMeal}`)}>
-                                    <Link id={`recipe:${recipe.strMeal}`} 
-                                    to= {{
-                                        pathname:'/Recipe',
-                                        state: {data: this.props.location.state.data, recipe: recipe}
-                                    }}>
-                                        {recipe.strMeal}
-                                    </Link>
-                                </button>)
-                                }
-                        </div>   
-                    </div>
-                    <div class="output" style={{backgroundImage: `url(${this.state.backgroundImg})`}}>
-                        <h1 id = "mealName">
-                          {this.state.head_text}
-                        </h1>
-                    </div>
-                </div>
-            </main>
-        )
-    }
+  render() {
+    return (
+      <main>
+        <div class="flex_container">
+          <div class="search_results_wrapper">
+            <form class="search_form" name="searchbar">
+              <input
+                class="search"
+                type="search"
+                placeholder="Search for a recipe"
+                id="search"
+                autofocus
+                required
+              />
+              <button type="submit" class="go_btn">
+                Go
+              </button>
+              <select name="catagory" id="catagory_drop" class="catagory_drop">
+                <option value="">Any Catagory</option>
+                <option value="Beef">Beef</option>
+                <option value="Breakfast">Breakfast</option>
+                <option value="Chicken">Chicken</option>
+                <option value="Dessert">Dessert</option>
+                <option value="Goat">Goat</option>
+                <option value="Lamb">Lamb</option>
+                <option value="Miscellaneous">Miscellaneous</option>
+                <option value="Pasta">Pasta</option>
+                <option value="Pork">Pork</option>
+                <option value="Seafood">Seafood</option>
+                <option value="Side">Side</option>
+                <option value="Starter">Starter</option>
+                <option value="Vegan">Vegan</option>
+                <option value="Vegetarian">Vegetarian</option>
+              </select>
+              <select name="region" id="region_drop" class="region_drop">
+                <option value="">Any Area</option>
+                <option value="American">American</option>
+                <option value="British">British</option>
+                <option value="Canadian">Canadian</option>
+                <option value="Chinese">Chinese</option>
+                <option value="Dutch">Dutch</option>
+                <option value="Egyptian">Egyptian</option>
+                <option value="French">French</option>
+                <option value="Greek">Greek</option>
+                <option value="Indian">Indian</option>
+                <option value="Irish">Irish</option>
+                <option value="Italian">Italian</option>
+                <option value="Jamaican">Jamaican</option>
+                <option value="Japanese">Japanese</option>
+                <option value="Kenyan">Kenyan</option>
+                <option value="Malaysian">Malaysian</option>
+                <option value="Mexican">Mexican</option>
+                <option value="Moroccan">Moroccan</option>
+                <option value="Polish">Polish</option>
+                <option value="Portuguese">Portuguese</option>
+                <option value="Russian">Russian</option>
+                <option value="Spanish">Spanish</option>
+                <option value="Thai">Thai</option>
+                <option value="Tunisian">Tunisian</option>
+                <option value="Turkish">Turkish</option>
+                <option value="Unknown">Unknown</option>
+                <option value="Vietnamese">Vietnamese</option>
+              </select>
+            </form>
+            <div class="results_btn">
+              {this.props.location.state.data.map((recipe) => (
+                <button
+                  id={`${recipe.strMeal}`}
+                  type="submit"
+                  class="btn border-primary btn-light btn-lg btn-block"
+                  onMouseOver={() => this.over(`${recipe.strMeal}`)}
+                >
+                  <Link
+                    id={`recipe:${recipe.strMeal}`}
+                    to={{
+                      pathname: "/Recipe",
+                      state: {
+                        data: this.props.location.state.data,
+                        recipe: recipe,
+                      },
+                    }}
+                  >
+                    {recipe.strMeal}
+                  </Link>
+                </button>
+              ))}
+            </div>
+          </div>
+          <div
+            class="output"
+            style={{ backgroundImage: `url(${this.state.backgroundImg})` }}
+          >
+            <h1 id="mealName">{this.state.head_text}</h1>
+          </div>
+        </div>
+      </main>
+    );
+  }
 }
 
 export default Search;
 
 /*
-strMeal =           meal name
-strCategory =       main ingredient
-strArea =           region
-strInstructions
-strMealThumb =      meal image
-strIngredient<num> =ingredient <num>, only up to 20
-strMeasure<num> =   measurement of ingredient <num>, only up to 20
-
 TODO:
     crashes if data is null (since map is called on data)
     buttons don't overflow properly
-    button hover doesn't display in output
-    button click doesn't go to recipe page
     make it work on smaller screens
 
-    buttons have strMealThumb as background image
     buttons have outline
-    output has dynamic information
 
 
                         <div class="results_btn">
@@ -154,25 +158,6 @@ TODO:
                             )}
                           }
                         </div>   
-
-
-    condRenBut(data) {
-      if (data === null) {
-        return <p>No</p>
-      }
-      else{
-        data.map(recipe => 
-          <button 
-            type="button" 
-            class="btn border-primary btn-light btn-lg btn-block" 
-            onClick>{recipe.strMeal}
-          </button>
-        )
-      }
-    }
-
-
-
 
 TO GO IN LANDING:
                         <br></br>
@@ -222,7 +207,4 @@ TO GO IN LANDING:
                             <option value="Unknown">Unknown</option>
                             <option value="Vietnamese">Vietnamese</option>                               
                         </select>
-                        {this.state.clicked? 
-                        <Search data={this.state.recipes}/> :
-                        null}
 */

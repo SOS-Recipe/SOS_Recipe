b12be610646ca9f7835c8e6164bf93eeb009beda
rivera26 <rivera26@pdx.edu>
2021-03-09 22:37:04 -0800

    Fixing express / react routing

--- server.js
+++ server.js
@@ -10,6 +10,13 @@ app.get('/getContacts', (req, res) => {
   res.json(contacts);
 })
 
+app.get('/*', (req, res) => {
+  let url = path.join(__dirname, '../frontend/build', 'index.html');
+  if (!url.startsWith('/components/'))
+    url = url.substring(1);
+  res.sendFile(url);
+});
+
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 })

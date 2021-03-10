50da818da6a16fee13365070cbe321d9cb1086fe
rivera26 <rivera26@pdx.edu>
2021-03-09 22:27:30 -0800

    Reverted changes

--- server.js
+++ server.js
@@ -3,27 +3,12 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 5000;
 
-//app.use(express.static(path.join(__dirname, 'frontend/build')));
+app.use(express.static(path.join(__dirname, 'frontend/build')));
 
 app.get('/getContacts', (req, res) => {
   var contacts = ["Sam Youngs", "David Froman", "Jonathan Rivera"];
   res.json(contacts);
 })
-
-let protected = ['transformed.js', 'main.css', 'favicon.ico']
-
-app.get("*", (req, res) => {
-
-  let path = req.params['0'].substring(1)
-
-  if (protected.includes(path)) {
-    // Return the actual file
-    res.sendFile(`${__dirname}/build/${path}`);
-  } else {
-    // Otherwise, redirect to /build/index.html
-    res.sendFile(`${__dirname}/build/index.html`);
-  }
-});
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);

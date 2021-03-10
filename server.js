52cf50480d1363cb77ba4822154391250b9a98ab
rivera26 <rivera26@pdx.edu>
2021-03-09 22:43:58 -0800

    Update server.js
    still trying to get routing working on the heroku deployment

--- server.js
+++ server.js
@@ -1,20 +1,33 @@
 const express = require('express');
-const path = require('path');
+//const path = require('path');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(express.static(path.join(__dirname, 'frontend/build')));
+//app.use(express.static(path.join(__dirname, 'frontend/build')));
 
 app.get('/getContacts', (req, res) => {
   var contacts = ["Sam Youngs", "David Froman", "Jonathan Rivera"];
   res.json(contacts);
 })
 
-app.get('/*', (req, res) => {
-  let url = path.join(__dirname, '../frontend/build', 'index.html');
-  if (!url.startsWith('/components/'))
-    url = url.substring(1);
-  res.sendFile(url);
+// app.get('/*', (req, res) => {
+//   let url = path.join(__dirname, '../frontend/build', 'index.html');
+//   if (!url.startsWith('/components/'))
+//     url = url.substring(1);
+//   res.sendFile(url);
+// });
+
+let protected = ['transformed.js', 'main.css', 'favicon.ico']
+
+app.get("*", (req, res) => {
+
+  let path = req.params['0'].substring(1)
+
+  if (protected.includes(path)) {
+    res.sendFile(`${__dirname}/build/${path}`);
+  } else {
+    res.sendFile(`${__dirname}/build/index.html`);
+  }
 });
 
 app.listen(port, () => {

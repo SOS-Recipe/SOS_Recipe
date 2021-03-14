761da31536141b3a01ee210719a433ba17bd3ee3
rivera26 <74120411+rivera26@users.noreply.github.com>
2021-03-13 19:36:49 -0800

    Update aboutTextJonathan.jsx

--- frontend/src/writing/aboutTextJonathan.jsx
+++ frontend/src/writing/aboutTextJonathan.jsx
@@ -1,4 +1,4 @@
-const aboutTextJonathan =`Put Your text Here`
+const aboutTextJonathan =`Jonathan is a current CS graduate student at PSU. He has a BA in Anthrolopology and Visual & Dramatic Art from Rice University. His hobbies include video games, photography, and music.`
 
 
 export default aboutTextJonathan;
